2c6a11bb62da705a238547580913d0ccefd39425
niLPotential <niLPotential@proton.me>
2025-04-07 13:03:46 +0900

    Add deno as extra package

--- hm/zed-editor.nix
+++ hm/zed-editor.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   options = {
     zed-editor.enable = lib.mkEnableOption "enables zed-editor";
@@ -9,6 +14,9 @@
       extensions = [
         "astro"
         "deno"
+      ];
+      extraPackages = with pkgs; [
+        deno
       ];
       userSettings = {
         buffer_line_height = "standard";
