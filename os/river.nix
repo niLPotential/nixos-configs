3c9aeccc492881da88ea2aece80367c15aaae796
niLPotential <niLPotential@proton.me>
2025-04-12 20:06:18 +0900

    Break theme name

--- os/river.nix
+++ os/river.nix
@@ -25,7 +25,7 @@
     programs.regreet = {
       enable = true;
       theme.package = pkgs.rose-pine-gtk-theme;
-      theme.name = "rose-pine";
+      theme.name = "rose-pin";
     };
     stylix.targets.regreet = {
       enable = false;
