91046733887d7ae4f909035f74eba57ec48df013
niLPotential <niLPotential@proton.me>
2025-04-13 15:46:45 +0900

    Try dark

--- os/river.nix
+++ os/river.nix
@@ -25,7 +25,7 @@
     programs.regreet = {
       enable = true;
       # theme.package = pkgs.adw-gtk3;
-      theme.name = "adw-gtk3";
+      theme.name = "adw-gtk3-dark";
     };
     stylix.targets.regreet = {
       enable = false;
