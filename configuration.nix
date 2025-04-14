409043e8d5d18d325d844899e9cf0d27b527adfc
niLPotential <niLPotential@proton.me>
2025-04-14 11:09:10 +0900

    Enable gtk debug

--- configuration.nix
+++ configuration.nix
@@ -35,6 +35,7 @@
   environment.variables = {
     QT_IM_MODULE = "fcitx";
     GTK_IM_MODULE = "fcitx";
+    GTK_DEBUG = "all";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
