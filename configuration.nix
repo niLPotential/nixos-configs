250e83c38c5cf0f134854b1d2fdb8c74352d8164
niLPotential <niLPotential@proton.me>
2025-05-25 11:06:53 +0900

    Disable autoupgrade

--- configuration.nix
+++ configuration.nix
@@ -100,9 +100,6 @@
     "flakes"
   ];
 
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
